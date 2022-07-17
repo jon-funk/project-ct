00d3880de52d5bc6034218a502c1d4a198490d7d
ezrajames <ezra@revela.io>
2022-07-17 16:56:36 -0700

    feat: Add working navigation header

--- app/web/pages/_app.js
+++ app/web/pages/_app.js
@@ -2,12 +2,11 @@ import '../styles/globals.css'
 import { AuthProvider } from "../contexts/auth";
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <AuthProvider>
       <Component {...pageProps} />
-      </AuthProvider>
-  )
+    </AuthProvider>
+  );
 }
 
 export default MyApp
