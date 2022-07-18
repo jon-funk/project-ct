a3c5e09eadd3f864ba53856d923352fa43cac62f
Brodie Hay <brodiehay2334@gmail.com>
2022-07-18 01:05:19 -0700

    form-submission: implement ability to submit a patient encounter form to the db

--- app/api/models/patient_encounter.py
+++ app/api/models/patient_encounter.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 from api.main.database import Base
 from api.models.mixins import BasicMetrics
-
+from api.schemas.patient_encounter import PatientEncounter
 
 class PatientEncounter(Base, BasicMetrics):
     __tablename__ = "patient_encounters"
@@ -17,15 +17,21 @@ class PatientEncounter(Base, BasicMetrics):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     patient_incident_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True)
     user_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True)
-    document_number = Column(String)
-    rfid = Column(String)
+    document_num = Column(String)
+    patient_rfid = Column(String)
+    location = Column(String)
     qr_code = Column(String)
     arrival_method = Column(String)
     on_shift = Column(Boolean)
-    triage_category = Column(String)
-    presenting_complaint = Column(String)
-    arrival_timestamp = Column(DateTime)
-    departure_timestamp = Column(DateTime)
+    triage_acuity = Column(String)
+    chief_complaints = Column(String)
+    arrival_time = Column(DateTime)
+    departure_time = Column(DateTime)
+    departure_dest = Column(String)
+    handover_from = Column(String)
+    date = Column(DateTime)
+    handover_too = Column(String)
+    comment = Column(String)
 
 def get_patient_encounter_by_id(db: Session, id: int) -> Union[PatientEncounter, None]:
     return db.query(PatientEncounter).filter(PatientEncounter.id == id).first()
@@ -33,13 +39,13 @@ def get_patient_encounter_by_id(db: Session, id: int) -> Union[PatientEncounter,
 def get_patient_encounter_by_document_number(db: Session, document_number: str) -> Union[PatientEncounter, None]:
     return db.query(PatientEncounter).filter(PatientEncounter.document_number == document_number).first()
 
-def create_patient_encounter(db: Session) -> PatientEncounter:
-    """Create a blank patient encounter with only a unique ID.
+def create_patient_encounter(db: Session, data: PatientEncounter) -> PatientEncounter:
+    """Create a patient encounter with a unique ID.
 
     Returns:
-        A PatientEncounter schema.
+        A PatientEncounter.
     """
-    created_patient_encounter = PatientEncounter()
+    created_patient_encounter = data
     db.add(created_patient_encounter)
     db.commit()
     db.refresh(created_patient_encounter)
--- app/web/pages/forms/form.js
+++ app/web/pages/forms/form.js
@@ -7,189 +7,283 @@ import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
-import useAuth, { ProtectedRoute } from '../../contexts/auth';
-import { ProtectedRoute } from '../../contexts/auth';
+import { useAuth, ProtectedRoute } from '../../contexts/auth';
 import ProtectedNavbar from "../../components/protected_navbar";
 
+import { submit_form } from "../../utils/api";
 
-function SMFPEForm() {
-  const [arrivalTime, setArrivalTime] = React.useState(new Date());
-  const [departureTime, setDepartureTime] = React.useState(new Date());
+ const MFPEFormData = {
+  patient_rfid: '',
+  document_num: '',
+  location: "Main Medical",
+  handover_from: '',
+  date: new Date,
+  arrival_time: new Date,
+  triage_acuity: '',
+  on_shift: '',
+  chief_complaints: [],
+  arrival_method: '',
+  handover_too: '',
+  departure_time: new Date,
+  departure_dest: '',
+  comment: '',
+}
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
+function MFPEForm() {
+  const [formValues, setFormValues] = React.useState(MFPEFormData);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
   };
 
-  const handleArrivalTimeChange = (newValue) => {
-    setArrivalTime(newValue);
-  };
+  const handleChiefComplaintsFieldChange = (event, value) => {
+    setFormValues({
+      ...formValues,
+      ["chief_complaints"]: value,
+    });
+  }
 
-  const handleDepartureTimeChange = (newValue) => {
-    setDepartureTime(newValue);
-  };
+  const handleArrivalTimeChange = (event) => {
+    setFormValues({
+      ...formValues,
+      ["arrival_time"]: event,
+    });  };
+
+  const handleDepartureTimeChange = (event) => {
+    setFormValues({
+      ...formValues,
+      ["departure_time"]: event,
+    });  };
+
+  const [errorMessage, setErrorMessage]  = React.useState("");
+  const [hasError, setError] = React.useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(false);
+    setErrorMessage("");
+    const data = formValues;
+
+    const errorMessage = await submit_form(data);
+    if (!errorMessage) {
+      window.location.pathname = "/forms/form";
+    } else {
+      setErrorMessage(errorMessage);
+      setError(true);
+    }
+  }
 
   return (
     <>
     <ProtectedNavbar/>
     <Container maxWidth="sm">
-      <FormGroup action="/send-data-here" method="post">
-        <Grid container spacing={2} style={{padding: 1 + 'rem'}}>
-          <Grid item xs={12}>
-            <h2>
-              <Link href="/">
-                <a>Music Festival Patient Encounter Form</a>
-              </Link>
-            </h2>
-          </Grid>
-          <Grid item xs={6}>
-            <InputLabel>Patient RFID: </InputLabel>
-            <TextField id="patient-rfid" label="Wristband RFID" variant="outlined" />
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Grid container spacing={2} style={{padding: 1 + 'rem'}}>
+            <Grid item xs={12}>
+              <h2>
+                <Link href="/">
+                  <a>Music Festival Patient Encounter Form</a>
+                </Link>
+              </h2>
             </Grid>
-          <Grid item xs={6}>
-            <InputLabel>Document #: </InputLabel>
-            <TextField id="document-number" label="eg. 1000" variant="outlined" />
-          </Grid>
-          <Grid item xs={6}>
-            <InputLabel id="location-select-label">Location</InputLabel>
-            <Select
-              labelId="location-select-label"
-              id="location-select"
-              defaultValue="Main Medical"
-              label="Location"
-              disabled
-              required
-              onChange={handleChange}>
-              <MenuItem value="Main Medical">Main Medical</MenuItem>
-            </Select>
-          </Grid>
-          <Grid item xs={6}>
-            <InputLabel>Handover From: </InputLabel>
-            <TextField id="handover-from" label="Who brought the patient" variant="outlined" required />
-          </Grid>
-          <Grid item xs={6}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <InputLabel>Date: </InputLabel>
-              <MobileDatePicker
-                inputFormat="MM/dd/yyyy"
+            <Grid item xs={6}>
+              <InputLabel>Patient RFID: </InputLabel>
+              <TextField
+                name="patient_rfid"
+                label="Wristband RFID"
+                variant="outlined"
+                value={formValues.rfid}
+                onChange={handleChange} />
+              </Grid>
+            <Grid item xs={6}>
+              <InputLabel>Document #: </InputLabel>
+              <TextField
+                name="document_num"
+                label="eg. 1000"
+                variant="outlined"
+                value={formValues.document_num}
+                onChange={handleChange} />
+            </Grid>
+            <Grid item xs={6}>
+              <InputLabel id="location-select-label">Location</InputLabel>
+              <Select
+                labelId="location-select-label"
+                name="location"
+                defaultValue="Main Medical"
+                label="Location"
+                disabled
                 required
-                value={arrivalTime}
-                onChange={handleArrivalTimeChange}
-                renderInput={(params) => <TextField {...params} />}
+                value={formValues.location}
+                onChange={handleChange}>
+                <MenuItem value="Main Medical">Main Medical</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={6}>
+              <InputLabel>Handover From: </InputLabel>
+              <TextField
+                name="handover_from"
+                label="Who brought the patient"
+                variant="outlined"
+                value={formValues.handover_from}
+                onChange={handleChange}
               />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={6}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <InputLabel>Arrival Time: </InputLabel>
-              <TimePicker
+            </Grid>
+            <Grid item xs={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <InputLabel>Date: </InputLabel>
+                <MobileDatePicker
+                  inputFormat="MM/dd/yyyy"
+                  required
+                  name="date"
+                  value={formValues.date}
+                  onChange={handleArrivalTimeChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <InputLabel>Arrival Time: </InputLabel>
+                <TimePicker
+                  required
+                  ampm={false}
+                  name="arrival_time"
+                  value={formValues.arrival_time}
+                  onChange={handleArrivalTimeChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Triage Acuity: </InputLabel>
+              <RadioGroup
+                aria-labelledby="triage-acuity"
+                name="triage_acuity"
+                row
                 required
-                ampm={false}
-                value={arrivalTime}
-                onChange={handleArrivalTimeChange}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-          </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Triage Acuity: </InputLabel>
-            <RadioGroup
-              aria-labelledby="triage-acuity"
-              name="triage-acuity"
-              row
-              required>
-              <FormControlLabel value="white" control={<Radio />} label="White" />
-              <FormControlLabel value="green" control={<Radio />} label="Green" />
-              <FormControlLabel value="yellow" control={<Radio />} label="Yellow" />
-              <FormControlLabel value="red" control={<Radio />} label="Red" />
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Is the patient a worker who is on shift: </InputLabel>
-            <RadioGroup
-              aria-labelledby="patient-Occupation"
-              name="checkbox-group"
-              row
-              required>
-              <FormControlLabel value="staff-yes" control={<Radio />} label="Yes" />
-              <FormControlLabel value="staff-no" control={<Radio />} label="No" />
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Chief Complaint: </InputLabel>
-            <Autocomplete
-              multiple
-              required
-              id="chief-complaints"
-              options={chiefComplaints}
-              disableCloseOnSelect
-              getOptionLabel={(option) => option.label}
-              renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                  <Checkbox
-                    checked={selected}
-                  />
-                  {option.label}
-                </li>
-              )}
-              renderInput={(params) => <TextField {...params} label="Select Complaint(s)" />}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Arrival Method: </InputLabel>
-            <RadioGroup
-              aria-labelledby="arrival-method"
-              name="radio-buttons-group"
-              row
-              required>
-              <FormControlLabel value="self-presented" control={<Radio />} label="Self Presented" />
-              <FormControlLabel value="med-transport" control={<Radio />} label="Medical Transport" />
-              <FormControlLabel value="security" control={<Radio />} label="Brought by Security" />
-              <FormControlLabel value="harm-reduction" control={<Radio />} label="Brought by Harm Reduction" />
-              <FormControlLabel value="other" control={<Radio />} label="Other"/>
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={6}>
-            <InputLabel>Handover To: </InputLabel>
-            <TextField id="handover-to" label="Patient is going with..." variant="outlined" />
-          </Grid>
-          <br/>
-          <Grid item xs={6}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <InputLabel>Departure Time: </InputLabel>
-              <TimePicker
+                value={formValues.triage_acuity}
+                onChange={handleChange}>
+                <FormControlLabel value="white" control={<Radio />} label="White" />
+                <FormControlLabel value="green" control={<Radio />} label="Green" />
+                <FormControlLabel value="yellow" control={<Radio />} label="Yellow" />
+                <FormControlLabel value="red" control={<Radio />} label="Red" />
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Is the patient a worker who is on shift: </InputLabel>
+              <RadioGroup
+                aria-labelledby="patient-Occupation"
+                name="on_shift"
+                row
                 required
-                ampm={false}
-                value={departureTime}
-                onChange={handleDepartureTimeChange}
-                renderInput={(params) => <TextField {...params} />}
+                value={formValues.on_shift}
+                onChange={handleChange}>
+                <FormControlLabel value="staff-yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="staff-no" control={<Radio />} label="No" />
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Chief Complaint: </InputLabel>
+              <Autocomplete
+                multiple
+                required
+                options={chiefComplaints}
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.label}
+                renderOption={(props, option, { selected }) => (
+                  <li {...props}>
+                    <Checkbox
+                      checked={selected}
+                    />
+                    {option.label}
+                  </li>
+                )}
+                renderInput={(params) => <TextField {...params}
+                  name="chief_complaints"
+                  label="Select Complaint(s)"
+                  value={formValues.chief_complaints}/>}
+                onChange={handleChiefComplaintsFieldChange}
+
               />
-            </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Arrival Method: </InputLabel>
+              <RadioGroup
+                aria-labelledby="arrival-method"
+                name="arrival_method"
+                row
+                required
+                value={formValues.arrival_method}
+                onChange={handleChange}>
+                <FormControlLabel value="self-presented" control={<Radio />} label="Self Presented" />
+                <FormControlLabel value="med-transport" control={<Radio />} label="Medical Transport" />
+                <FormControlLabel value="security" control={<Radio />} label="Brought by Security" />
+                <FormControlLabel value="harm-reduction" control={<Radio />} label="Brought by Harm Reduction" />
+                <FormControlLabel value="other" control={<Radio />} label="Other (Please explain in the comment section)"/>
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={6}>
+              <InputLabel>Handover To: </InputLabel>
+              <TextField
+               name="handover_too"
+               label="Patient is going with..."
+               variant="outlined"
+               value={formValues.handover_too}
+               onChange={handleChange} />
+            </Grid>
+            <br/>
+            <Grid item xs={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <InputLabel>Departure Time: </InputLabel>
+                <TimePicker
+                  required
+                  ampm={false}
+                  name="departure_time"
+                  value={formValues.departure_time}
+                  onChange={handleDepartureTimeChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Departure Destination: </InputLabel>
+              <RadioGroup
+                required
+                aria-labelledby="departure-destination"
+                name="departure_dest"
+                row
+                value={formValues.departure_dest}
+                onChange={handleChange}>
+                <FormControlLabel value="lwbs" control={<Radio />} label="LWBS" />
+                <FormControlLabel value="left-ama" control={<Radio />} label="Left AMA" />
+                <FormControlLabel value="return-to-event" control={<Radio />} label="Sanctuary" />
+                <FormControlLabel value="security" control={<Radio />} label="Security" />
+                <FormControlLabel value="hospital-private" control={<Radio />} label="Hospital by private car" />
+                <FormControlLabel value="hostpital-ambulance" control={<Radio />} label="Hospital by ambulance" />
+                <FormControlLabel value="other" control={<Radio />} label="Other (Please explain in the comment section)" />
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <InputLabel>Comments: </InputLabel>
+              <TextField
+                name="comment"
+                label="enter comments here..."
+                variant="outlined"
+                fullWidth={true}
+                value={formValues.comment}
+                onChange={handleChange}  />
+            </Grid>
+            <Grid item xs={12}>
+            <Button type="submit" fullWidth={true} variant="contained">Submit</Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Departure Destination: </InputLabel>
-            <RadioGroup
-              required
-              aria-labelledby="departure-destination"
-              name="radio-buttons-group"
-              row>
-              <FormControlLabel value="lwbs" control={<Radio />} label="LWBS" />
-              <FormControlLabel value="left-ama" control={<Radio />} label="Left AMA" />
-              <FormControlLabel value="return-to-event" control={<Radio />} label="Sanctuary" />
-              <FormControlLabel value="security" control={<Radio />} label="Security" />
-              <FormControlLabel value="hospital-private" control={<Radio />} label="Hospital by private car" />
-              <FormControlLabel value="hostpital-ambulance" control={<Radio />} label="Hospital by ambulance" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={12}>
-            <InputLabel>Comments: </InputLabel>
-            <TextField id="comments" label="enter comments here..." variant="outlined" fullWidth  />
-          </Grid>
-          <Grid item xs={12}>
-            <Button fullWidth="true" variant="contained">Submit</Button>
-          </Grid>
-        </Grid>
-      </FormGroup>
+        </FormGroup>
+      </form>
     </Container>
   </>
   );
@@ -214,4 +308,4 @@ const chiefComplaints = [
   { label: 'Trauma' , id: 16 },
 ];
 
-export default ProtectedRoute(SMFPEForm);
+export default ProtectedRoute(MFPEForm);
