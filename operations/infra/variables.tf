0e244fb680e7d3a18520f9b0027e0369b431c859
cmcaughren <cmcaughren@gmail.com>
2023-03-13 20:19:48 -0700

    Completed Task 2 terraform set up work.
    - cloud_run_service uses demo image from greenfield work
    - migrated terraform state files to Google cloud storage bucket
    - added cloudsql database

---
+++ operations/infra/variables.tf
@@ -0,0 +1,15 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "db_user" { }
+
+variable "db_user_password" {}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
