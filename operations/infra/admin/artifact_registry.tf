19453d1d694cf3d8f5ae5bf3e97ab2be3040acca
cmcaughren <cmcaughren@gmail.com>
2023-04-14 23:18:08 -0700

    Github action workflow, artifact registry variable updates.

--- operations/infra/admin/artifact_registry.tf
+++ operations/infra/admin/artifact_registry.tf
@@ -13,11 +13,5 @@ resource "google_artifact_registry_repository" "GAR" {
 resource "github_actions_secret" "GAR_secret" {
   repository       = var.project_repo
   secret_name      = "ARTIFACT_REGISTRY"
-  plaintext_value  = google_artifact_registry_repository.GAR.repository_id
+  plaintext_value  = "${local.GAR_location}${ local.GAR_name}/"
 }
-
-#resource "github_actions_secret" "example_secret" {
-#  repository       = "example_repository"
-#  secret_name      = "example_secret_name"
-#  encrypted_value  = var.some_encrypted_secret_string
-#}
