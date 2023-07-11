52ec1bd43e260094283ff7d71d421799a51d7172
Zeke Critchlow <critch646@gmail.com>
2023-07-10 22:42:03 -0700

    Zeke/dev/rfid api route (#53)
    * renamed files for clarity
    
    * added get latest patient encounter by rfid route
    
    * added get patient encounter by rfid unit tests

--- app/api/routes/patient_encounter/__init__.py
+++ app/api/routes/patient_encounter/__init__.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-from . import get_patient_encounter
+from . import get_latest_patient_encounter_rfid
+from . import get_patient_encounter_uuid
 from . import get_patient_encounters
 from . import submit_patient_encounter
-from . import delete_patient_encounter
+from . import delete_patient_encounter_uuid
 from . import update_patient_encounter
