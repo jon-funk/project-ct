44f70a761a9c321b053bb91d7efd514d41f3f1a9
Jonathan Funk <jonathan.funk@runwhen.com>
2023-07-17 20:24:34 -0700

    Wrap registration in admin mode flag

--- app/api/main/app.py
+++ app/api/main/app.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Session
 from api import routes, constants
 
 API_PREFIX = os.getenv("API_PREFIX")
+ADMIN_MODE = int(os.getenv("ADMIN_MODE", 0))
 
 fe_origin = os.environ.get("FRONTEND_ORIGIN")
 protocol = os.environ.get("PROTOCOL")
@@ -35,8 +36,13 @@ api.add_middleware(
     allow_headers=["*"],
 )
 
+# only allow registrations if the app is started in admin mode
+# default to off
+# quick hack until granular RBAC user permissions implemented
+if ADMIN_MODE == 1:
+    api.include_router(routes.register.router, prefix=API_PREFIX)
+
 api.include_router(routes.login.router, prefix=API_PREFIX)
-api.include_router(routes.register.router, prefix=API_PREFIX)
 api.include_router(routes.refresh_token.router, prefix=API_PREFIX)
 api.include_router(routes.patient_encounter.router, prefix=API_PREFIX)
 
