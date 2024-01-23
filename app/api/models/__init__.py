9e6c938149a8cf5fd81eff7e0a6b98ffe2b4ef8a
MadelaineJ <madelainejones@stumail.viu.ca>
2024-01-22 19:25:13 -0800

    added sanctuary intake model and schema to api
    Problem
    We need to add sanctuary to mediform
    
    Solution
    - added intake model
    - added intake schema
    
    Ticket URL
    
    Documentation
    
    Tests Run
    Tested that uncommenting the lines in models/__init__.py and then running migrations will add intake table

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -1,2 +1,6 @@
 from api.models.user import User
 from api.models.patient_encounter import PatientEncounter
+
+# TODO: uncomment this for sanctuary dev db
+# Ticket: https://mediform.atlassian.net/browse/MEDI-30
+# from api.models.intake import Intake
