04f4e711624cf8f57746060837b7c59c58d77705
Zeke Critchlow <critch646@gmail.com>
2024-01-28 11:43:46 -0800

    implement sanctuary intake listview page on frontend
    Problem
    Sanctuary needs a page that displays a list of all intake forms that
    have been submitted.
    
    Solution
    - Implemented MUI data grid to display intakes from database.
    - Added API call to fetch intakes from database. Has error handling.
    - Fake data is used for now, but will be replaced with real data once
      backend is implemented.
    - Each row in the data grid is clickable and will take the user to the
      intake form page for that intake.
    - The modify intake form page is not implemented yet, but will be
      implemented in a future ticket.
    
    Ticket URL
    https://mediform.atlassian.net/browse/MEDI-26
    
    Documentation
    N/A
    
    Tests Run
    - Tested in dev environment at http://localhost:3000/sanctuary/search
    - Data grid displays fake data
    - Each row is clickable and takes user to intake form page
    (not implemented yet)
    - Displays error message if API call fails (when using actual function
      instead of fake data)
    
    <details>
    <summary>Images</summary>
    
    ![image](https://github.com/jon-funk/project-ct/assets/22301841/04e98e77-1f2e-4dad-8fd5-85186c47e5ef)
    ![image](https://github.com/jon-funk/project-ct/assets/22301841/39bb7aa3-573f-4b9e-8cd6-a93e505a087c)
    </details>

--- app/web/pages/sanctuary/search.tsx
+++ app/web/pages/sanctuary/search.tsx
@@ -1,16 +1,150 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ProtectedRoute } from "../../contexts/auth";
+import { IntakeFormDataInterface } from "../../interfaces/IntakeFormDataInterface";
+import ProtectedNavbar from "../../components/ProtectedNavbar";
+import { AppBar, Box, Dialog, IconButton, Toolbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { DataGrid } from "@mui/x-data-grid";
+import { SlideTransition } from "../../components/SlideTransition";
+import { SanctuaryModifyIntakeForm } from "../../components/intake_form/SanctuaryModifyIntakeForm";
+import { fetchFakeSanctuaryIntakes } from "../../utils/api_sanctuary";
 
 
+/**
+ * SanctuaryIntakeSearch is the page that displays all the intake forms in the database.
+ * 
+ * @returns The page that displays all the intake forms in the database.
+ */
+function SanctuaryIntakeSearch(): React.JSX.Element {
 
-function SanctuaryIntakeSearch(): JSX.Element {
-    return (
-        <Container maxWidth="md">
-            <Typography variant="h1">Sanctuary Search</Typography>
-        </Container>
-    );
+    const [sanctuaryIntakes, setSanctuaryIntakes] = useState<IntakeFormDataInterface[]>([]);
+    const [isError, setIsErrorMessage] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedRow, setSelectedRow] = useState<IntakeFormDataInterface>({} as IntakeFormDataInterface);
+    const [formUUID, setFormUUID] = useState("");
+
+    const openDialogWindow = (rowData: { row: IntakeFormDataInterface }) => {
+        setOpenDialog(true);
+        setSelectedRow(rowData.row);
+        setFormUUID(rowData.row.intake_uuid);
+    };
+
+    const closeDialogWindow = () => {
+        setOpenDialog(false);
+        setSelectedRow({} as IntakeFormDataInterface);
+    };
+
+    useEffect(() => {
+        const token = `Bearer ${window.localStorage.getItem("auth-token")}`;
+        if (sanctuaryIntakes.length === 0) {
+            // TODO: Replace with fetchSanctuaryIntakes when API is ready
+            fetchFakeSanctuaryIntakes(token, setSanctuaryIntakes, setIsErrorMessage, setErrorMessage);
+        }
+    }, [sanctuaryIntakes]);
+
+    const columns = [
+        { field: "intake_uuid", headerName: "UUID", width: 200 },
+        { field: "guest_rfid", headerName: "Guest RFID", width: 150 },
+        { field: "arrival_date", headerName: "Arrival Date", width: 100 },
+        { field: "arrival_time", headerName: "Arrival Time", width: 150 },
+        { field: "arrival_method", headerName: "Arrival Method", width: 150 },
+        { field: "identified_gender", headerName: "Identified Gender", width: 200 },
+        { field: "first_visit", headerName: "First Visit", width: 200 },
+        { field: "presenting_complaint", headerName: "Presenting Complaint", width: 200 },
+        { field: "guest_consciousness_level", headerName: "Guest Consciousness Level", width: 150 },
+        { field: "guest_emotional_state", headerName: "Guest Emotional State", width: 250 },
+        { field: "substance_categories", headerName: "Substance Categories", width: 250 },
+        { field: "time_since_last_dose", headerName: "Time Since Last Dose", width: 250 },
+        { field: "departure_date", headerName: "Departure Date", width: 250 },
+        { field: "departure_time", headerName: "Departure Time", width: 250 },
+        { field: "departure_dest", headerName: "Departure Destination", width: 250 },
+    ];
+
+    if (isError) {
+        return (
+            <>
+                <ProtectedNavbar />
+                <Container>
+                    {isError && (
+                        <Typography
+                            variant="h4"
+                            component="h4"
+                            sx={{
+                                pt: "2rem",
+                                margin: "2rem",
+                                textAlign: "center",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            {"ERROR: " + errorMessage}
+                        </Typography>
+                    )}
+                </Container>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <ProtectedNavbar />
+                <Container>
+                    <Typography
+                        variant="h4"
+                        component="h4"
+                        sx={{
+                            pt: "2rem",
+                            margin: "2rem",
+                            textAlign: "center",
+                            fontWeight: "bold",
+                        }}
+                    >
+                        All Intake Forms
+                    </Typography>
+                    <Box sx={{ height: 600, width: "100%" }}>
+                        <DataGrid
+                            rows={sanctuaryIntakes}
+                            columns={columns}
+                            onRowClick={openDialogWindow}
+                            pageSize={10}
+                            getRowId={(row) => row.intake_uuid}
+                            rowsPerPageOptions={[20]}
+                            checkboxSelection={false}
+                            disableSelectionOnClick={true}
+                        />
+                    </Box>
+
+                    <Dialog fullScreen open={openDialog} TransitionComponent={SlideTransition}>
+                        <AppBar sx={{ position: "fixed" }}>
+                            <Toolbar>
+                                <Typography
+                                    sx={{ ml: 2, flex: 1, fontWeight: "bold" }}
+                                    variant="h6"
+                                    component="div"
+                                >
+                                    UUID: {formUUID}
+                                </Typography>
+                                <IconButton
+                                    edge="start"
+                                    color="inherit"
+                                    onClick={closeDialogWindow}
+                                    aria-label="close"
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                            </Toolbar>
+                        </AppBar>
+                        {
+                            openDialog && (
+                                <SanctuaryModifyIntakeForm formUUID={formUUID} rowData={selectedRow} />
+                            )
+                        }
+                    </Dialog>
+                </Container>
+            </>
+        );
+    }
 }
 
 export default ProtectedRoute(SanctuaryIntakeSearch);
