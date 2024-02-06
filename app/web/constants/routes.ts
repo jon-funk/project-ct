994f166810955e791ca702fceb3b78ec283f8a38
Zeke Critchlow <critch646@gmail.com>
2024-02-05 19:54:25 -0800

    update protectednavbar to use usergroup
    Problem
    The existing navbar was hardcoded for medical user group.
    
    Solution
    - Use user group at login to dynamically change the links in the navbar
    - Add a new constant file to store the user group keys
    - Add functions in authentication for storing/retrieving user group from
    local storage.
        + To avoid server-side rendering issues, these functions check if they
    are running on the client side before trying to access local storage.
    - Navbar checks current route path and if user not from that user group
    it will redirect to the home page
    
    Ticket URL
    https://mediform.atlassian.net/browse/MEDI-34
    
    Documentation
    N/A
    
    Tests Run
    - Locally executed `yarn build` and built with no errors
    - Ran local dev environment and tested the navbar with different user groups
        + Verified that the navbar links change based on user group
        + Verified that the navbar redirects to the home page if the user is not
        from the user group of the current route

--- app/web/constants/routes.ts
+++ app/web/constants/routes.ts
@@ -1,5 +1,14 @@
-export const RoutesMedical = {
-  home: "/medical/home",
-  form: "/medical/form",
-  search: "/medical/search",
+export const UserGroupRoutes = {
+  medical: {
+    home: "/medical/home",
+    form: "/medical/form",
+    search: "/medical/search",
+  },
+  sanctuary: {
+    home: "/sanctuary/home",
+    form: "/sanctuary/form",
+    search: "/sanctuary/search",
+  },
 };
+
+export type UserGroupKey = keyof typeof UserGroupRoutes;
