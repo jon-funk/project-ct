8334e760dbf839c4692928b27cfa9c43f0da443c
MadelaineJ <68253853+MadelaineJ@users.noreply.github.com>
2024-02-09 15:45:22 -0800

    Added more unit tests for sign up (#97)
    * Added more unit tests for sign up
    
    Problem
    Test coverage was limited to just the happy path
    
    Solution
    - added test for no user group
    - added test for invalid user group
    - added test for user already existing
    
    Ticket URL
    NA
    
    Documentation
    NA
    Tests Run
    - make all && make api && make testapi
    
    * addressing pr comments

--- app/api/tests/test_register.py
+++ app/api/tests/test_register.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 
 from api.constants import MEDICAL
 from api.models.user import create_user
-from api.main.database import SessionLocal
+from api.main.database import db_functions
 
 
 @pytest.mark.needs(postgres=True)
@@ -27,3 +27,75 @@ def test_valid_register(client: TestClient) -> None:
     assert (
         "access_token" in resp_data
     ), "Access token was not provided after signing up a valid user"
+
+
+@pytest.mark.needs(postgres=True)
+def test_no_usergroup_register(client: TestClient) -> None:
+    """
+    Test that a user without a user group can't make an account
+    """
+
+    resp = client.post(
+        "/api/sign-up",
+        json={
+            "email": "nogroup@gmail.com",
+            "password": "Testing-123",
+        },
+    )
+    resp_data = resp.json()
+
+    assert (
+        resp.status_code == 400
+    ), "Invalid user group error should have been returned, instead got:\n" + str(
+        resp_data
+    )
+
+
+@pytest.mark.needs(postgres=True)
+def test_invalid_user_group_register(client: TestClient) -> None:
+    """
+    Test that a user with an invalid user group can't make an account
+    """
+
+    resp = client.post(
+        "/api/sign-up",
+        json={
+            "email": "nogroup@gmail.com",
+            "password": "Testing-123",
+            "user_group": "invalidGroup",
+        },
+    )
+    resp_data = resp.json()
+
+    assert (
+        resp.status_code == 400
+    ), "Invalid user group error should have been returned, instead got:\n" + str(
+        resp_data
+    )
+
+
+@pytest.mark.needs(postgres=True)
+def test_existing_user_register(client: TestClient) -> None:
+    """
+    Test that an existing user can't make a new account
+    """
+    db_generator = db_functions[MEDICAL]()
+    db = next(db_generator)
+    user = create_user(db, email="testValid@gmail.com", password="Testing-123")
+
+    assert user, "User not created before trying to sign in a valid user."
+    resp = client.post(
+        "/api/sign-up",
+        json={
+            "email": "testValid@gmail.com",
+            "password": "Testing-123",
+            "user_group": MEDICAL,
+        },
+    )
+    resp_data = resp.json()
+
+    assert (
+        resp.status_code == 409
+    ), "User already exists error should have been returned, instead got:\n" + str(
+        resp_data
+    )
