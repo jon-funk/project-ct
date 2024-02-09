54f4c669e673b2cb24564881d7e92fae7d6a937e
MadelaineJ <68253853+MadelaineJ@users.noreply.github.com>
2024-02-09 14:13:09 -0800

    updating sign-up route to handle user groups (#95)
    Problem
    sign up route doesn't handle or expect a user group
    
    Solution
    - sign-up now expects user group
    - sign-up puts user in appropriate db given user_group
    - created 1 unit test for valid sign in
    - TODO: write tests for invalid sign in
    
    Ticket URL
    https://mediform.atlassian.net/browse/MEDI-58
    
    Documentation
    NA
    
    Tests Run
    - make all && make api && make testapi

--- app/api/routes/register.py
+++ app/api/routes/register.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from api.schemas.users import UserLogin, UserLoginResponse
 from api.models.user import create_user, get_user_by_email
 from api.main.auth import generate_auth_token
-from api.main.database import get_db
+from api.main.database import db_functions
 
 router = APIRouter()
 
@@ -14,22 +14,32 @@ router = APIRouter()
 @router.post(
     "/sign-up", status_code=200, response_model=UserLoginResponse, name="sign-up"
 )
-def sign_up(user: UserLogin, db: Session = Depends(get_db)) -> Any:
+def sign_up(user: UserLogin) -> Any:
     """
     Create a new user if a user with that email doesn't exist. Otherwise raise exceptions.
     """
+    # get the correct db given the user group
+    if user.user_group and user.user_group in db_functions:
+        db_generator = db_functions[user.user_group]()
+        db = next(db_generator)
+    else:
+        raise HTTPException(status_code=400, detail="Invalid user group.")
+
     found_user = get_user_by_email(db, str(user.email))
     if found_user:
         raise HTTPException(
             status_code=409, detail="User with that email already exists."
         )
 
-    user = create_user(db, email=str(user.email), password=user.password)
-    if not user:
+    # otherwise if user doesn't exist
+    newUser = create_user(db, email=str(user.email), password=user.password)
+    if not newUser:
         raise HTTPException(
             status_code=500, detail="Unable to create user at this time."
         )
 
-    token = generate_auth_token(data={"sub": user.email})
+    token = generate_auth_token(
+        data={"sub": newUser.email}, user_group=user.user_group
+    )
 
     return UserLoginResponse(access_token=token)
