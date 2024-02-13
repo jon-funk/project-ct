4c29f750bfe7ce78702c5a457197e90fff35b931
MadelaineJ <68253853+MadelaineJ@users.noreply.github.com>
2024-02-13 09:43:09 -0800

    Added new routes for sanctuary (#101)
    Problem
    Sanctuary requires routes
    
    Solution
    - Created route files
    - replaced medical related functions with intake function
    - TODO: write unit tests and manually test routes
    
    Ticket URL
    NA
    
    Documentation
    NA
    
    Tests Run
    make all && make testapi

---
+++ app/api/routes/intakes/get_intake_uuid.py
@@ -0,0 +1,46 @@
+import logging
+from uuid import UUID
+
+from fastapi import Depends, HTTPException
+
+from sqlalchemy.orm import Session
+
+from . import router
+
+from api.main.database import get_db_sanctuary as get_db
+from api.models.intake import get_intake_by_uuid
+from api.schemas.intake import IntakeResponseSchema
+
+LOGGER = logging.getLogger(__name__)
+
+
+@router.get(
+    "/intake",
+    status_code=200,
+    response_model=IntakeResponseSchema,
+    name="get-intake",
+)
+def get_intake(
+    uuid: UUID,
+    db: Session = Depends(get_db),
+) -> IntakeResponseSchema:
+    """
+    Retrieve a intake from the database with the provided UUID.
+    """
+    try:
+        intake = get_intake_by_uuid(db, uuid)
+    except Exception as err:
+        LOGGER.error(
+            f"Server error while trying to get intakes: {err}",
+        )
+        raise HTTPException(
+            status_code=500,
+            detail="Unable to get intakes at this time. Please try again later or contact support.",
+        )
+
+    if not intake:
+        raise HTTPException(
+            status_code=404, detail="Unable to find an entry with that document id."
+        )
+
+    return IntakeResponseSchema.from_orm(intake)
