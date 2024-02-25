80463cc32db5dbe87d26e4f62e25a0b9dc8e20f1
MadelaineJ <68253853+MadelaineJ@users.noreply.github.com>
2024-02-25 11:01:57 -0800

    Created sanctuary test db (#109)
    Problem
    Sanctuary unit tests require their own test db
    
    Solution
    - updated init-db.sh script to create medical and sanctuary test db
    - updated database.py to check if we're running unit tests
    - removed setting POSTGRES_DB=example_test as it's no longer needed
    
    Ticket URL
    NA
    Documentation
    NA
    Tests Run
    NA

--- app/api/main/database.py
+++ app/api/main/database.py
@@ -11,20 +11,33 @@ from api.config import load_env
 
 
 load_env()
+test = os.environ.get("IS_TEST")
 user = os.environ.get("POSTGRES_USER")
 password = os.environ.get("POSTGRES_PASSWORD")
 hostname = os.environ.get("POSTGRES_HOST")
 port = str(os.environ.get("POSTGRES_PORT"))
-db = os.environ.get("POSTGRES_DB")
+db_medical = os.environ.get("POSTGRES_DB")
+db_sanctuary = os.environ.get("POSTGRES_SANCTUARY_DB")
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{db}"
-)
 
-# Define engine for the 'sanctuary' database
-SQLALCHEMY_DATABASE_URL_SANCTUARY = (
-    f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/sanctuary"
-)
+if test:
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{db_medical}_test"
+    )
+
+    SQLALCHEMY_DATABASE_URL_SANCTUARY = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{db_sanctuary}_test"
+    )
+else:
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{db_medical}"
+    )
+    # Define engine for the 'sanctuary' database
+    SQLALCHEMY_DATABASE_URL_SANCTUARY = (
+        f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{db_sanctuary}"
+    )
+
+
 engine_sanctuary = create_engine(SQLALCHEMY_DATABASE_URL_SANCTUARY)
 SessionLocalSanctuary = sessionmaker(
     autocommit=False, autoflush=False, bind=engine_sanctuary
@@ -75,9 +88,9 @@ db_functions = {
 
 def create_all_tables() -> None:
     Base.metadata.create_all(engine_example, checkfirst=True)
-#    BaseSanctuary.metadata.create_all(engine_example, checkfirst=True)
+    BaseSanctuary.metadata.create_all(engine_sanctuary, checkfirst=True)
 
 
 def drop_all_tables(check_first: bool = False) -> None:
     Base.metadata.drop_all(engine_example, checkfirst=check_first)
-#    BaseSanctuary.metadata.drop_all(engine_example, checkfirst=check_first)
+    BaseSanctuary.metadata.drop_all(engine_sanctuary, checkfirst=check_first)
--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -2,5 +2,13 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE sanctuary;
+  CREATE DATABASE "sanctuary-example";
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE "sanctuary-example_test";
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE example_test;
 EOSQL
