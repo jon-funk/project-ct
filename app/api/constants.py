5521079a2a992a4b467a9b0af74dc1fe4584e5ae
Zeke Critchlow <critch646@gmail.com>
2024-02-27 09:01:17 -0800

    updated load_current_user in api to check user group in list (#111)
    Problem
    - The API was not authenticating sanctuary users because
     load_current_user was only checking if the user group was medical
    
    Solution
    - Updated the load_current_user() function to check if the user group is in the list of allowed groups
    
    Ticket
    N/A
    
    Tests
    - docker compose up testapi
    - manually logged in as a sanctuary user and checked frontend behaviour

--- app/api/constants.py
+++ app/api/constants.py
@@ -3,3 +3,4 @@ API_DESCRIPTION = "Template API demonstrating FastAPI basics"
 API_VERSION = "0.1.0"
 MEDICAL = "medical"
 SANCTUARY = "sanctuary"
+USER_GROUPS = [MEDICAL, SANCTUARY]
--- app/api/main/auth.py
+++ app/api/main/auth.py
@@ -8,17 +8,16 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
-from api.constants import MEDICAL, SANCTUARY
+from api.constants import MEDICAL, SANCTUARY, USER_GROUPS
 from api.main.database import db_functions
 from api.models.user import get_user_by_email
 
 
 security = HTTPBearer()
 
-# The medical load user function
+
 def load_current_user(
-    request: Request,
-    credentials: HTTPAuthorizationCredentials = Security(security)
+    request: Request, credentials: HTTPAuthorizationCredentials = Security(security)
 ) -> T.Any:
     """
     Try and decrypt a JSON token and return the corresponding
@@ -31,21 +30,24 @@ def load_current_user(
     try:
         payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=["HS256"])
         email: str = payload.get("sub")
-        user_group: str = payload.get("user_group")  # Extract user group from the token
-        if email is None or user_group != MEDICAL:
+        user_group: str = payload.get("user_group")
+        if email is None or user_group not in USER_GROUPS:
             raise get_credentials_exception()
     except JWTError:
         raise get_credentials_exception()
+
     if user_group in db_functions:
         db_generator = db_functions[user_group]()
         db = next(db_generator)
     else:
         raise HTTPException(status_code=400, detail="Invalid user group.")
+
     user = get_user_by_email(db, email)
     if user is None:
         raise get_credentials_exception()
 
     return user
+
 
 # get the user group. required for refreshing the token
 def get_user_group(
@@ -59,7 +61,7 @@ def get_user_group(
         user_group: str = payload.get("user_group")  # Extract user group from the token
     except JWTError:
         raise get_credentials_exception()
-    
+
     return user_group
 
 
@@ -100,7 +102,9 @@ def verify_password(password_hash: str, password: str) -> bool:
     return argon2.verify(password, password_hash)
 
 
-def generate_auth_token(data: dict, user_group: str, expires_minutes: int = 60 * 24) -> str:
+def generate_auth_token(
+    data: dict, user_group: str, expires_minutes: int = 60 * 24
+) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
     to_encode.update(
