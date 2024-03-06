b3a618f8029c612a532d5a50ea3fa08dc51f150e
Zeke Critchlow <critch646@gmail.com>
2024-03-06 12:31:46 -0800

    sanctuary frontend intergrated with api (#122)
    Problem
    - Sanctuary functionality was not fully implemented or integrated with the api
    
    Solution
    - Update new intake page and API calls
    - List intakes now makes API calls
    - Update intake implemented and integrated with API
    - Intakes can now be deleted through frontend using the API
    
    Ticket:
    https://mediform.atlassian.net/browse/MEDI-75
    
    Documentation:
    N/A
    
    Tests
    - yarn build passes
    - manual testing on local dev environment - create new intake - view intakes - update existing intake - delete intake

--- app/web/interfaces/IntakeFormDataInterface.ts
+++ app/web/interfaces/IntakeFormDataInterface.ts
@@ -16,5 +16,23 @@ export interface IntakeFormDataInterface {
   departure_time: Date;
   departure_date: Date;
   departure_dest: string;
-  departure_dest_other: string;
+  departure_dest_other?: string;
+}
+
+export interface APIIntakeFormData {
+  guest_rfid: string;
+  arrival_date: string;
+  arrival_time: string;
+  arrival_method: string;
+  identified_gender: string;
+  first_visit: boolean;
+  presenting_complaint: string;
+  guest_consciousness_level: string;
+  guest_emotional_state: string;
+  substance_categories: string;
+  time_since_last_dose: number;
+  discharge_date: string;
+  discharge_time: string;
+  discharge_method: string;
+  intake_uuid: string;
 }
