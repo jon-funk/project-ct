41e8d6fdc32db10563b57c5d9475c421dc8ea04c
Zeke Critchlow <critch646@gmail.com>
2024-03-09 18:30:12 -0800

    add offsite transports chart to post festival dashboard
    Problem
    - The post festival dashboard does not include a chart for offsite transports
    
    Solution
    - Add a chart for offsite transports to the post festival dashboard
    - Created transport type color palette
    
    Ticket:
    N/A
    
    Documentation:
    N/A
    
    Tests
    - manual testing in local dev environment
    - `yarn build` passes

--- app/web/components/dashboard/PostFestivalDashboards.tsx
+++ app/web/components/dashboard/PostFestivalDashboards.tsx
@@ -10,8 +10,8 @@ import { LengthOfStayCountsTableProps } from "../../interfaces/LengthOfStayCount
 import { TopTenCommonPresentationsTableProps } from "../../interfaces/TopTenCommonPresentationsTableProps";
 import { AcuityCountsData } from "../../interfaces/AcuityCountsData";
 import { AcuityCountPerDay } from "../../interfaces/PosteventDashboard";
-import { PatientEncounterCountByDayStackedBarChart, PatientEncounterCountByDayTable } from "./PatientEncounterCountsByDay";
-import { triageColorStyles } from "../../constants/colorPalettes";
+import { PatientEncounterCountByDayStackedBarChart, PatientEncounterCountByDayTable, OffsiteTransportBreakdownSideBarChart } from "./PatientEncounterCountsByDay";
+import { triageColorStyles, offsiteTransportColorStyles } from "../../constants/colorPalettes";
 
 interface PostFestivalSummaryProps {
     selectedYear: string;
@@ -94,8 +94,26 @@ export const PatientEncountersDashboardComponent: React.FC<PatientEncountersDash
 };
 
 export const OffsiteTransportsDashboardComponent = () => {
-    return <Box>
-    </Box>;
+
+    const offsiteTransportCounts = {
+        ambulance: 3,
+        private: 4,
+        nonEmergency: 7,
+    }
+
+    return <Grid container spacing={2} style={{ padding: "1rem", justifyContent: "center" }}>
+        <Grid item xs={12} md={6} lg={5} xl={3} sx={{ maxWidth: "400px" }}>
+            <OffsiteTransportBreakdownSideBarChart offsiteTransportCounts={offsiteTransportCounts} />
+        </Grid>
+        <Grid item xs={12} md={6} lg={5} xl={3} sx={{ maxWidth: "400px" }}>
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={4} xl={2} sx={{ maxWidth: "400px" }}>
+        </Grid>
+        <Grid item xs={2}>
+            <OffsiteTransportLegend transportColorStyles={offsiteTransportColorStyles} />
+        </Grid>
+    </Grid>
 }
 
 export const PatientLengthOfStayDashboardComponent = () => {
@@ -103,13 +121,13 @@ export const PatientLengthOfStayDashboardComponent = () => {
     </Box>;
 }
 
-interface TriageColorStyle {
+interface ColorStyle {
     backgroundColor: string;
     color: string;
 }
 
 interface TriageAcuityLegendProps {
-    triageColorStyles: { [key: string]: TriageColorStyle };
+    triageColorStyles: { [key: string]: ColorStyle };
     legendStyle?: React.CSSProperties;
 }
 
@@ -118,7 +136,7 @@ export const TriageAcuityLegend: React.FC<TriageAcuityLegendProps> = ({ triageCo
     const boxHeight = 25;
     const boxWidth = 75;
     const spaceBetweenBoxes = 5;
-    const titlePaddingTop = 10;
+    const titlePaddingTop = 15;
     const svgWidth = Math.max(200, boxWidth);
 
     const svgHeight = Object.keys(triageColorStyles).length * (boxHeight + spaceBetweenBoxes) + titlePaddingTop * 2;
@@ -136,6 +154,43 @@ export const TriageAcuityLegend: React.FC<TriageAcuityLegendProps> = ({ triageCo
                     <g key={level} transform={`translate(${(svgWidth - boxWidth) / 2}, ${yPosition})`}>
                         <rect width={boxWidth} height={boxHeight} fill={triageColorStyles[level].backgroundColor} stroke="black" />
                         <text x={boxWidth / 2} y={boxHeight / 2} textAnchor="middle" dy="0.35em" fill={triageColorStyles[level].color}>
+                            {level.charAt(0).toUpperCase() + level.slice(1)}
+                        </text>
+                    </g>
+                );
+            })}
+        </svg>
+    );
+}
+
+interface OffsiteTransportLegendProps {
+    transportColorStyles: { [key: string]: ColorStyle };
+    legendStyle?: React.CSSProperties;
+}
+
+export const OffsiteTransportLegend: React.FC<OffsiteTransportLegendProps> = ({ transportColorStyles }) => {
+
+    const boxHeight = 25;
+    const boxWidth = 120;
+    const spaceBetweenBoxes = 5;
+    const titlePaddingTop = 15;
+    const svgWidth = Math.max(200, boxWidth);
+
+    const svgHeight = Object.keys(transportColorStyles).length * (boxHeight + spaceBetweenBoxes) + titlePaddingTop * 2;
+
+    return (
+        <svg width={svgWidth} height={svgHeight} style={{ display: "block", margin: "auto" }}>
+            {/* Title */}
+            <text x={svgWidth / 2} y={titlePaddingTop} textAnchor="middle" fontWeight="bold" fontSize="16">
+                Offsite Transport Legend
+            </text>
+            {/* Legend boxes */}
+            {Object.keys(transportColorStyles).map((level, index) => {
+                const yPosition = index * (boxHeight + spaceBetweenBoxes) + titlePaddingTop * 2;
+                return (
+                    <g key={level} transform={`translate(${(svgWidth - boxWidth) / 2}, ${yPosition})`}>
+                        <rect width={boxWidth} height={boxHeight} fill={transportColorStyles[level].backgroundColor} stroke="black" />
+                        <text x={boxWidth / 2} y={boxHeight / 2} textAnchor="middle" dy="0.35em" fill={transportColorStyles[level].color}>
                             {level.charAt(0).toUpperCase() + level.slice(1)}
                         </text>
                     </g>
