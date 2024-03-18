98e3e32c7fee446cda45bb0ed3468959212bda88
MadelaineJ <madelainejones@stumail.viu.ca>
2024-03-18 09:28:58 -0700

    replacing return HTTPException with raise
    Problem
    Fastapi standard is to use raise instead of return
    
    Solution
    there was only one instance (that I could find) where we used return instead of raise
    
    Ticket URL
    https://mediform.atlassian.net/browse/MEDI-46
    
    Documentation
    NA
    
    Tests Run
    make testapi

--- app/api/main/auth.py
+++ app/api/main/auth.py
@@ -85,7 +85,7 @@ def generate_auth_token(
 
 def get_credentials_exception() -> HTTPException:
 
-    return HTTPException(
+    raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
